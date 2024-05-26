141422b98da6f0cd80e8afd2392151e4698669b4
Rahul <7678478061k@gmail.com>
2024-05-26 13:55:17 +0530

    added basic layout to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,30 @@
+import Header from "@/components/home/header";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
-  return <div>Home Page</div>
+  return <div className="h-screen w-full border flex flex-col" >
+    <Header />
+
+    <main className="flex-1">
+      <section className="border px-60 flex h-full items-center">
+        <div className="flex gap-8">
+          <div>
+            <Image src={'welcome-icon.svg'} width={20} height={20} alt="welcome-icon" className="w-full" />
+          </div>
+          <div className="flex flex-col gap-8 border">
+            <div>
+              <h2 className="text-2xl text-slate-200">Welcome to <b>DevSync</b></h2>
+              <p className="text-justify py-2">Your go-to platform for real-time project collaboration. Easily work on projects, modify code, and perform CRUD operations on files and folders all within one interface.</p>
+            </div>
+            <div>
+              <Button variant='default'>Create Project</Button>
+              <Button variant="outline">Join Project</Button>
+            </div>
+          </div></div>
+      </section>
+    </main>
+
+    <footer className="border w-full bg-emerald-400 p-8"></footer>
+  </div>
 }
---
+++ components/home/header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+
+    return <header className="w-full border p-4">
+
+        <div className="logo">
+            <h1 className="font-semibold text-3xl">Dev Sync</h1>
+        </div>
+
+    </header>
+}
