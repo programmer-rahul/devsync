888b24c9d8e076aa554d9869e655a83d1d245646
Rahul <7678478061k@gmail.com>
2024-05-27 12:19:12 +0530

    added dialog for join project btn

--- components/client/home/join-project.tsx
+++ components/client/home/join-project.tsx
@@ -1,12 +1,91 @@
 "use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
+import { FormEvent, useState } from "react";
 
 export default function JoinProjectBtn() {
-  const joinProjectHandler = () => {};
+  const [joinProjectValues, setJoinProjectValues] = useState({
+    username: "",
+    projectId: "",
+  });
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (
+      joinProjectValues.username.trim() === "" ||
+      joinProjectValues.projectId.trim() === ""
+    )
+      return;
+
+    console.log("submitted", joinProjectValues);
+    setIsDialogOpen(false);
+  };
 
   return (
-    <Button variant="outline" size="lg" onClick={joinProjectHandler}>
-      Join Project
-    </Button>
+    <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+      <DialogTrigger asChild>
+        <Button variant="outline" size="lg">
+          Join Project
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <form onSubmit={handleSubmit}>
+          <DialogHeader>
+            <DialogTitle>Join Project</DialogTitle>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="username" className="text-left">
+                User Name
+              </Label>
+              <Input
+                id="username"
+                placeholder="dev123"
+                value={joinProjectValues.username}
+                onChange={(event) =>
+                  setJoinProjectValues((prev) => ({
+                    ...prev,
+                    username: event.target.value.toLocaleLowerCase(),
+                  }))
+                }
+                className="col-span-3"
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="projectId" className="text-left">
+                Project Id
+              </Label>
+              <Input
+                id="projectId"
+                placeholder="paste here..."
+                value={joinProjectValues.projectId}
+                onChange={(event) =>
+                  setJoinProjectValues((prev) => ({
+                    ...prev,
+                    projectId: event.target.value,
+                  }))
+                }
+                className="col-span-3"
+              />
+            </div>
+          </div>
+          <DialogFooter>
+            <Button type="submit">Join</Button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
   );
 }
