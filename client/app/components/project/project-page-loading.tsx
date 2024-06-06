da98ff69254d4169f24fe83b9d9abef7bcc1829c
Rahul <7678478061k@gmail.com>
2024-06-06 22:53:13 +0530

    changed loading text and project not available

--- client/app/components/project/project-page-loading.tsx
+++ client/app/components/project/project-page-loading.tsx
@@ -1,7 +1,8 @@
 export default function ProjectPageLoading() {
   return (
-    <div className="flex w-full items-center justify-center text-6xl">
+    <div className="flex w-full items-center justify-center text-6xl flex-col ">
       <div className="loading-animation"/>
+      <div>Connection to Project</div>
     </div>
   );
 }
--- client/app/components/project/project-page-not-available.tsx
+++ client/app/components/project/project-page-not-available.tsx
@@ -4,8 +4,8 @@ export default function ProjectPageIsNotAvailable() {
   return (
     <div className="flex w-full items-center justify-center ">
       <div className="flex flex-col items-center gap-4">
-        <h3 className="text-4xl">This Project is not available</h3>
-        <p className="text-2xl">Reason :- Wrong ProjectId</p>
+        <h3 className="text-5xl">No Project Found</h3>
+        <p className="text-3xl">wrong projectid</p>
         <GobackOnHomePageBtn />
       </div>
     </div>
