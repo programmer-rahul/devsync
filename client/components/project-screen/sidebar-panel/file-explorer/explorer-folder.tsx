b2bbcd0472e0ed059460d9fe4c99857386128bf6
Rahul <7678478061k@gmail.com>
2024-07-05 12:44:35 +0530

    refactor: seperate explorerFolder functions

--- client/components/project-screen/sidebar-panel/file-explorer/explorer-folder.tsx
+++ client/components/project-screen/sidebar-panel/file-explorer/explorer-folder.tsx
@@ -2,16 +2,10 @@
 
 import { cn } from "@/lib/utils";
 import { Folder as FolderInterface } from "@/types/explorer";
-import { useEffect, useRef, useState } from "react";
-import ExplorerFile from "./explorer-file";
 import ExplorerFolderName from "./explorer-folder-name";
-import { Input } from "@/components/ui/input";
-import { useStore } from "@/components/store/useStore";
 
-import useProjectCrud from "@/hooks/useProjectCrud";
-import { FaFolder } from "react-icons/fa";
-import { getLanguageIcon } from "@/lib/editor/get-language-icon";
-import NewExplorerItemCreationInput from "./new-explorer-item-creation-input";
+import ExplorerSubFolders from "./explorer-sub-folders";
+import { useState } from "react";
 
 export default function ExplorerFolder({
   id: folderId,
@@ -19,13 +13,6 @@ export default function ExplorerFolder({
   files,
   subFolders,
 }: FolderInterface) {
-  // zunstand store states
-  const selectedFolderId = useStore((state) => state.selectedFolderId);
-  const creatingProjectItem = useStore((state) => state.creatingProjectItem);
-  const updateCreatingProjectItem = useStore(
-    (state) => state.updateCreatingProjectItem,
-  );
-
   // to maintain folder collapsing state
   const [isCollapsed, setIsCollapsed] = useState(false);
 
@@ -40,49 +27,16 @@ export default function ExplorerFolder({
       />
 
       {/* subfolders and files  */}
-      <div
-        className={cn(
-          "block",
-          folderId !== ":root" && !isCollapsed && "hidden",
-        )}
-      >
-        {/* subfolders  */}
-        {subFolders?.map(({ id, name, type, files, subFolders }) => {
-          return (
-            <ExplorerFolder
-              key={id}
-              id={id}
-              name={name}
-              type={type}
-              files={files}
-              subFolders={subFolders}
-            />
-          );
-        })}
-
-        {/* new project item creation input  */}
-        {creatingProjectItem.status && selectedFolderId === folderId && (
-          <NewExplorerItemCreationInput
-            folderName={folderName}
-            folderId={folderId}
-            isCollapsed={isCollapsed}
-            setIsCollapsed={setIsCollapsed}
-          />
-        )}
-
-        {/* files  */}
-        {files?.map(({ id, name, type, content }) => {
-          return (
-            <ExplorerFile
-              key={id}
-              id={id}
-              name={name}
-              type={type}
-              content={content}
-            />
-          );
-        })}
-      </div>
+      {subFolders && subFolders.length > 0 && (
+        <ExplorerSubFolders
+          isCollapsed={isCollapsed}
+          setIsCollapsed={setIsCollapsed}
+          folderName={folderName}
+          folderId={folderId}
+          files={files || []}
+          subFolders={subFolders}
+        />
+      )}
     </div>
   );
 }
